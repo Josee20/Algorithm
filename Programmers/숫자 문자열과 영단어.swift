d7aea158e1cd75618f2b96b9133688942dc9cc72
dlee <dlehdrl1115@naver.com>
2025-08-08 23:33:59 +0900

    숫자 문자열과 영단어

---
+++ Programmers/숫자 문자열과 영단어.swift
@@ -0,0 +1,12 @@
+//
+//  숫자 문자열과 영단어.swift
+//  Algorithm
+//
+//  Created by 이동기 on 8/8/25.
+//
+
+import Foundation
+
+class 숫자_문자열과_영단어 {
+    
+}
--- Solution/main.swift
+++ Solution/main.swift
@@ -7,5 +7,42 @@
 
 import Foundation
 
+class Main {
+    func solution(_ s:String) -> Int {
+        var result: String = ""
+        let dict: [String: String] = [
+            "zero": "0",
+            "one": "1",
+            "two": "2",
+            "three": "3",
+            "four": "4",
+            "five": "5",
+            "six": "6",
+            "seven": "7",
+            "eight": "8",
+            "nine": "9"
+        ]
+        
+        var str = ""
+        
+        for char in s {
+            str += String(char)
+            
+            if let numStr = dict[str] {
+                result += numStr
+                str = ""
+            } else if char.isNumber {
+                result += String(char)
+                str = ""
+            }
+        }
+        
+        return Int(result)!
+    }
+}
 
-
+let main = Main()
+print(main.solution("one4seveneight"))
+print(main.solution("23four5six7"))
+print(main.solution("2three45sixseven"))
+print(main.solution("123"))
